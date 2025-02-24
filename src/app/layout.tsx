7415d0083dc67d8482c244bcc3a0b72ded5976e6
SinhBX <buisinh25042001@gmail.com>
2025-02-24 23:32:58 +0700

    setup môi trường, làm giao diện navbar

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+export const metadata: Metadata = {
+  title: "Aribnb",
+  description: "Aribnb clone",
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+   <h2 className="text-red-500"></h2>
+  );
+}
